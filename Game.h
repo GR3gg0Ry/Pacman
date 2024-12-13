ca81cd0cda3140cf61b785cc43d8d140d1270cec
GR3gg0Ry <grigorii55frolov@gmail.com>
2024-12-14 00:21:42 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -3,15 +3,20 @@
 #include "Map.h"
 #include "Entity.h"
 #include "Input.h"
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 class Game {
 public:
     Game();
+    int* menu();
     void pacmanMove();
     void ghostMove();
     void output();
     bool pacmanIsExist();
     void printGameOverScreen();
+    int options();
+    int bonuses();
 private:
     Map map_;
     Pacman pacman_;
