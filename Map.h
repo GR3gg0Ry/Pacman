fcbec91428d5861186b53c27b776bec357a175d1
GR3gg0Ry <grigorii55frolov@gmail.com>
2025-01-08 00:13:58 +0300

    Update Map.h

--- Map.h
+++ Map.h
@@ -2,56 +2,38 @@
 
 #include <string>
 #include <vector>
-#include <fstream>
-#include <cstring>
 
 #include "GameUtils.h"
 #include "ScoreCounter.h"
-#define MAX_NAME 10
+#include "Render.h"
 
 class Map {
 public:
-    Map();
-    char getField(std::size_t x, std::size_t y);
+    Map(const std::vector<std::string>& map);
+
+    char getField(Point p) const;
+    void changeField(Point p, char symbol);
+
     Size getMapSize();
-    void changeField(std::size_t x, std::size_t y, char symbol);
-    Point getPacmanStartCoordinates();
-    std::vector<Point> getGhostStartCoordinates();
+
+    Point getPacmanStartCoordinates() const;
+    std::vector<Point> getGhostStartCoordinates() const;
+
+    Point findOtherPortal(Point cur_portal);
+
     void addScores(int scores);
-    void renderMap();
-    void Map_change(char*);
-private:
-    std::vector<std::string> map_ = {
-        "#############################",
-        "#...............#........O..#",
-        "#O###.#####.###.#.###.###.#.#",
-        "#.###.#.....#...#...#.....#.#",
-        "#.###.#.###.###.###.#.###.#.#",
-        "#.....#.#.....G.....#.#.....#",
-        "#.###.#.#.#########.#.#.###.#",
-        "#.###.#.#...........#.#.###.#",
-        "#.....#.#.###.###.###.#.....#",
-        "#####.#.#.#.......#.#.#.#####",
-        "#.......#.#.......#.#.......#",
-        "#.#######.#...P...#.#######.#",
-        "T...........................T",
-        "#####.#.#.#.......#.#.#.#####",
-        "#.....#.#.#.###.###.#.#...G.#",
-        "#.###.#.#...........#.#.###.#",
-        "#.###.#.#.#########.#.#.###.#",
-        "#.O...#G#...........#.#.....#",
-        "#.###.#.###.###.###.#.###.#.#",
-        "#.###.#.....#...#...#.....#.#",
-        "#.###.#####.###.#.###.###.#.#",
-        "#...............#......O....#",
-        "#############################"
-    };
+
+    void printMap();
     
+private:
+    std::vector<std::string> map_;
+    ScoreCounter score_counter_;
     Size map_size_;
     Point pacman_start_coordinates_;
     std::vector<Point> ghost_start_coordinates_;
-    ScoreCounter score_counter_;
-    bool checkField(std::size_t x, std::size_t y);
+
+    bool checkField(Point p) const;
+    
     Point findPacmanStartCoordinates();
     std::vector<Point> findGhostStartCoordinates();
 };
