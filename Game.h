28aa2d738f66474f679914627f6ccbb0dcb4fb79
GR3gg0Ry <grigorii55frolov@gmail.com>
2025-01-08 00:11:27 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -3,33 +3,32 @@
 #include "Map.h"
 #include "Entity.h"
 #include "Input.h"
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-
+#include "Render.h"
+#include "GameMenu.h"
 
 class Game {
 public:
-    Game();
-    int* menu(int);
+    Game(Map map, Configuration config);
+
     void pacmanMove();
     void ghostMove();
-    void output();
+
     bool pacmanIsExist();
-    void printGameOverScreen();
-    int options();
-    int bonuses();
-    int map(int);
-    int Map_counter();
-    
+
+    void gameOver();
+    void printMap();
+
 private:
     Map map_;
     Pacman pacman_;
     std::vector<Ghost> ghosts_;
     Input input_;
-    Point findOtherPortal(const Point& current_portal);
-    bool isOccupiedByGhost(const Point& point);
-    char fileNames[MAX_NAME][MAX_NAME];
+    Configuration configuration_;
+
+    std::vector<GhostBehavior> ghost_behaviors_ = {
+        FOLLOW_PACMAN,
+        RANDOM
+    };
+
+    bool isOccupiedByGhost(Point p);
 };
