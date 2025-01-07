379a487e6253ee47832a782c3319f6cf121f18cf
GR3gg0Ry <grigorii55frolov@gmail.com>
2025-01-08 00:15:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include <unistd.h>
 
+#include "GameMenu.h"
 #include "Game.h"
 
 using namespace std;
 
 int main() {
-    
-    while(true) {
-    Game game;
-    int* options = game.menu(game.Map_counter());
-    
     while (true) {
-        
-        game.output();
-        
-        if (game.pacmanIsExist()) {
-            game.pacmanMove();
-        } else {
-            game.printGameOverScreen();
-            sleep(3);
-            break;
-        }
+        GameMenu menu;
+        Map map(menu.mainMenu());
+        Configuration config = menu.getConfig();
 
-        if (game.pacmanIsExist()) {
-            game.ghostMove();
-        } else {
-            game.printGameOverScreen();
-            sleep(3);
-            break;
-        }
-    } }
+        Game game(map, config);
+        while (true) {
+            
+            game.printMap();
+            
+            if (game.pacmanIsExist()) {
+                game.pacmanMove();
+            } else {
+                game.gameOver();
+                sleep(3);
+                break;
+            }
+
+            if (game.pacmanIsExist()) {
+                game.ghostMove();
+            } else {
+                game.gameOver();
+                sleep(3);
+                break;
+            }
+        } 
+    }
     return 0;
 }
